e7a79de88fdf3ccfa76c896e45cf09c2904fb039
Milovan Marrder <milovanmarrder@outlook.com>
2026-02-13 15:34:38 -0600

    bug: add init imports in transform

---
+++ src/mmarrder/transform/__init__.py
@@ -0,0 +1,13 @@
+from .periodos import (
+    DICT_MESES,
+    recortar_por_ultima_linea,
+    normalizar_periodos,
+    filtrar_periodo,
+)
+
+__all__ = [
+    "DICT_MESES",
+    "recortar_por_ultima_linea",
+    "normalizar_periodos",
+    "filtrar_periodo",
+]
