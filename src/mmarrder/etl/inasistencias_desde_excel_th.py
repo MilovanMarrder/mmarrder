fff244001b3cc7fa379fcf1b1f20878f63459b47
Milovan Marrder <milovanmarrder@outlook.com>
2026-03-25 11:38:53 -0600

    Fix permisos_etl

--- src/mmarrder/etl/inasistencias_desde_excel_th.py
+++ src/mmarrder/etl/inasistencias_desde_excel_th.py
@@ -91,8 +91,19 @@ def vacaciones_etl(
     df_final.drop(columns=['dias_vacaciones'], inplace=True)
     return df_final
 
-def permisos_etl(ruta_archivo, sheet_name,fecha_corte = "2026-01-31"):
-    df = pd.read_excel(ruta_archivo, sheet_name=sheet_name, skiprows=1, dtype={'COD. EMPL':str})
+def permisos_etl(
+    ruta_archivo_permiso,
+    sheet_name_permisos,
+    fecha_minima="2026-01-01",
+    fecha_maxima="2100-12-31",
+):
+    df = pd.read_excel(
+        ruta_archivo_permiso,
+        sheet_name=sheet_name_permisos,
+        skiprows=1,
+        dtype={"COD. EMPL": str},
+    )
+    # renombro columnas para estandarizar con el proceso de vacaciones
     df.columns = [
         "area_gestion",
         "id_empleado",
@@ -110,66 +121,80 @@ def permisos_etl(ruta_archivo, sheet_name,fecha_corte = "2026-01-31"):
         "a",
         "b",
         "c",
-        "d"
+        "d",
     ]
-    # Trabajaré primero los diarios y luegos los permisos por horas
-    df_diarios = df[df['fecha_inicio'].notna()].copy()
-    df_diarios['fecha_inicio'] = pd.to_datetime(df_diarios['fecha_inicio'], errors='coerce')
-    df_diarios['fecha_fin'] = pd.to_datetime(df_diarios['fecha_fin'], errors='coerce')
-    fecha_corte = pd.to_datetime(fecha_corte)
-    df_diarios["dias_permiso_hasta_fecha_corte"] = df_diarios.apply(
-        lambda fila: len(
-            pd.bdate_range(
-                start=fila["fecha_inicio"],
-                end=fecha_corte if pd.isna(fila["fecha_fin"]) 
-                                else min(fila["fecha_fin"], fecha_corte)
-            )
+    # Filtrar solo los permisos que tengan fecha de inicio dentro del periodo
+    df["fecha_inicio"] = pd.to_datetime(df["fecha_inicio"], errors="coerce")
+    df = df[
+        (df["fecha_inicio"].notna())
+        & (df["fecha_inicio"] >= fecha_minima)
+        & (df["fecha_inicio"] <= fecha_maxima)
+    ]
+
+    df["fecha_inicio"] = pd.to_datetime(
+        df["fecha_inicio"], errors="coerce"
+    )
+    df["fecha_fin"] = pd.to_datetime(df["fecha_fin"], errors="coerce")
+    fecha_corte = pd.to_datetime(fecha_maxima)
+    df["fecha_fin_dentro_periodo"] = df.apply(
+        lambda row: (
+            fecha_maxima if row["fecha_fin"] > fecha_maxima else row["fecha_fin"]
         ),
-        axis=1
+        axis=1,
     )
 
-    df_diarios["tipo_inasistencia"] = "permiso diario"
-    puestos_seis_horas = [
-        "MEDICO ESPECIALISTA",
-        "MEDICO ESPECIALISTA EN CUIDADOS INTENSIVOS",
-        "CIRUJANO PEDIATRICO",
-        "INMUNOLOGIA",
-        "NEUROLOGA",
-        "MEDICO GENERAL",
+    df["dias_inasistencia_en_periodo"] = df.apply(
+        lambda row: tabla_calendario(
+            fecha_inicio=row["fecha_inicio"], fecha_fin=row["fecha_fin_dentro_periodo"]
+        ).dias_laborales(),
+        axis=1,
+    )
+
+    df["dias_inasistencia_en_periodo"] = df.apply(
+        lambda row: (
+            row["dias_inasistencia_en_periodo"]
+            if row["dias_permiso"] >= 1
+            else row["dias_permiso"]
+        ),
+        axis=1,
+    )
+    df["inicio_datetime"] = pd.to_datetime(
+        df["fecha_permiso"].astype(str)
+        + " "
+        + df["permiso_hora_inicio"].astype(str),
+        errors="coerce",
+    )
+
+    df["fin_datetime"] = pd.to_datetime(
+        df["fecha_permiso"].astype(str)
+        + " "
+        + df["permiso_hora_fin"].astype(str),
+        errors="coerce",
+    )
+    df["horas_decimal"] = (
+        df["fin_datetime"] - df["inicio_datetime"]
+    ).dt.total_seconds() / 3600
+    df["dias_inasistencia_en_periodo"] = df.apply(
+        lambda row: (
+            row["horas_decimal"] / 8
+            if (row["dias_inasistencia_en_periodo"] == 1)
+            and (pd.notna(row["permiso_hora_inicio"]))
+            else row["dias_inasistencia_en_periodo"]
+        ),
+        axis=1,
+    )
+    df = df[
+        [
+            "area_gestion",
+            "id_empleado",
+            "nombre",
+            "puesto",
+            "fecha_permiso",
+            "fecha_inicio",
+            "fecha_fin",
+            "tipo_permiso",
+            "dias_inasistencia_en_periodo",
+        ]
     ]
-    df_diarios['horas_decimal'] = df_diarios.apply(
-        lambda fila: 6*fila['dias_permiso_hasta_fecha_corte'] if fila['puesto'] in puestos_seis_horas else 8*fila['dias_permiso_hasta_fecha_corte'],
-        axis=1
-    )
 
-    df_horas = df[df['permiso_hora_inicio'].notna()].copy()
-
-    df_horas["inicio_datetime"] = pd.to_datetime(
-        df_horas["fecha_permiso"].astype(str) + " " + df_horas["permiso_hora_inicio"].astype(str)
-    )
-
-    df_horas["fin_datetime"] = pd.to_datetime(
-        df_horas["fecha_permiso"].astype(str) + " " + df_horas["permiso_hora_fin"].astype(str)
-    )
-    df_horas["horas_decimal"] = (
-        (df_horas["fin_datetime"] - df_horas["inicio_datetime"])
-        .dt.total_seconds() / 3600
-    )
-
-
-    cols_a_concatenar = [
-        "area_gestion",
-        "id_empleado",
-        "nombre",
-        "puesto",
-        "fecha_permiso",
-        "tipo_permiso",
-        "horas_decimal",
-    ]
-    horas_permiso = pd.concat(
-        [df_diarios[cols_a_concatenar], df_horas[cols_a_concatenar]], ignore_index=True
-    )
-    
-    
-
-    return horas_permiso
+    return df
